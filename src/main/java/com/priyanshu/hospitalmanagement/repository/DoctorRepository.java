dfc0a031e828de6aa7053645ad61c974f7d0a270
Priyanshu Jaiswal <yourgithubemail@gmail.com>
2026-04-02 17:54:50 +0530

    "fix: Admin page"

--- src/main/java/com/priyanshu/hospitalmanagement/repository/DoctorRepository.java
+++ src/main/java/com/priyanshu/hospitalmanagement/repository/DoctorRepository.java
@@ -1,30 +1,62 @@
+//package com.priyanshu.hospitalmanagement.repository;
+//
+//import com.priyanshu.hospitalmanagement.entity.Department;
+//import com.priyanshu.hospitalmanagement.entity.Doctor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+//    Optional<Doctor> findByNameIgnoreCase(String name);
+//    List<Doctor> findByNameContainingIgnoreCase(String name);
+//
+//
+//        long count();
+//    List<Doctor> findBySpecializationIgnoreCase(String specialization);
+//
+//    Page<Doctor> findByNameContainingIgnoreCase(String name, Pageable pageable);
+//
+//    Page<Doctor> findBySpecializationIgnoreCase(String specialization, Pageable pageable);
+//
+//    Page<Doctor> findByNameContainingIgnoreCaseAndSpecializationIgnoreCase(
+//            String name,
+//            String specialization,
+//            Pageable pageable);
+//
+//    List<Doctor> findByDepartments(Department department);
+//}
 package com.priyanshu.hospitalmanagement.repository;
 
 import com.priyanshu.hospitalmanagement.entity.Department;
 import com.priyanshu.hospitalmanagement.entity.Doctor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+//    @EntityGraph(attributePaths = {"departments"})
+//    @Override
+//    Page<Doctor> findAll(Pageable pageable);
+
     Optional<Doctor> findByNameIgnoreCase(String name);
     List<Doctor> findByNameContainingIgnoreCase(String name);
 
-
-        long count();
+    long count();
     List<Doctor> findBySpecializationIgnoreCase(String specialization);
 
     Page<Doctor> findByNameContainingIgnoreCase(String name, Pageable pageable);
-
     Page<Doctor> findBySpecializationIgnoreCase(String specialization, Pageable pageable);
 
     Page<Doctor> findByNameContainingIgnoreCaseAndSpecializationIgnoreCase(
-            String name,
-            String specialization,
-            Pageable pageable);
+            String name, String specialization, Pageable pageable);
 
     List<Doctor> findByDepartments(Department department);
 }
