1780c4c0860055ccadfea94d5733ce62c6b2ec64
Priyanshu Jaiswal <yourgithubemail@gmail.com>
2026-04-03 15:10:53 +0530

    "fix:Add medicine Enum "

--- src/main/java/com/priyanshu/hospitalmanagement/entity/type/MedicineType.java
+++ src/main/java/com/priyanshu/hospitalmanagement/entity/type/MedicineType.java
@@ -1,12 +1,5 @@
 package com.priyanshu.hospitalmanagement.entity.type;
 
 public enum MedicineType {
-
-    TABLET,
-    CAPSULE,
-    SYRUP,
-    INJECTION,
-    OINTMENT,
-    DROPS
-
+    DROPS, TABLET, INJECTION, CAPSULE, OINTMENT, CREAM, SYRUP, INHALER
 }
