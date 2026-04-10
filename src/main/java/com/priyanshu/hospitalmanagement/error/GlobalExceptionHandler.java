eee80589d5684985ae6a3fc87d50e9e4470d659c
Priyanshu Jaiswal <yourgithubemail@gmail.com>
2026-04-10 20:02:09 +0530

    change made here

--- src/main/java/com/priyanshu/hospitalmanagement/error/GlobalExceptionHandler.java
+++ src/main/java/com/priyanshu/hospitalmanagement/error/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
-
+//sd
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
