7046a24b68794d97f1c13911083ccc0625d131ba
Priyanshu Jaiswal <yourgithubemail@gmail.com>
2026-04-13 02:12:58 +0530

    "Kafka + NotificationDispatcher + Docker working"

--- src/main/java/com/priyanshu/hospitalmanagement/config/KafkaTopicConfig.java
+++ src/main/java/com/priyanshu/hospitalmanagement/config/KafkaTopicConfig.java
@@ -1,10 +1,12 @@
 package com.priyanshu.hospitalmanagement.config;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@ConditionalOnProperty(name = "kafka.enabled", havingValue = "true")
 public class KafkaTopicConfig {
 
     public static final String APPOINTMENT_EMAIL_TOPIC = "appointment-email";
